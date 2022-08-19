2e92d1857407a983aab5c20e3ec306d15a6f01bf
Sahil <sahilvaktepatil123@gmail.com>
2022-08-19 11:28:34 +0530

    Show Product API

--- controllers/Addproduct.js
+++ controllers/Addproduct.js
@@ -1,34 +1,49 @@
-const Products = require("../models/products")
+const Products = require("../models/products");
 const bcrypt = require("bcryptjs");
+const { response } = require("express");
 
+// ADD Products
 const addproducts = (req, res, next) => {
+  let products = new Products({
+    name: req.body.name,
+    desc: req.body.desc,
+    category: req.body.category,
+    price: req.body.price,
+    // userId:
+  });
 
-  
-
-      let products = new Products({
-        name: req.body.name,
-        desc: req.body.desc,
-        category: req.body.category,
-        price: req.body.price,
-        // userId:
+  products
+    .save()
+    .then((user) => {
+      res.json({
+        message: "Product Added Successfully !",
       });
+    })
+    .catch((error) => {
+      console.log("Error", error);
+      res.json({
+        message: "An Error Occured !!!",
+      });
+    });
+};
 
-      products
-        .save()
-        .then((user) => {
-          res.json({
-            message: "Product Added Successfully !",
-          });
-        })
-        .catch((error) => {
-          console.log("Error",error)
-          res.json({
-            message: "An Error Occured !!!",
-          });
-        });
-    
-  };
+const index =(req,res,next)=>{
+  Products.find()
+  .then(response =>{
+    res.json({
+      response
+    })
+  })
+  .catch(error =>{
+    res.json({
+      message:'An error Occured'
+    })
+  })
+}
 
-  module.exports = {
-    addproducts
-  }
+
+
+module.exports = {
+  addproducts,
+  index,
+};
--- routes/prodauth.js
+++ routes/prodauth.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const ProdController = require('../controllers/Addproduct')
 
 router.post('/addproduct',ProdController.addproducts)
-
+router.get('/',ProdController.index)
 
 module.exports=router
