b9c10f978fbac64bd2170472e011271a2277c870
Sahil <sahilvaktepatil123@gmail.com>
2022-08-19 11:39:37 +0530

    showproduct

--- routes/prodauth.js
+++ routes/prodauth.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const ProdController = require('../controllers/Addproduct')
 
 router.post('/addproduct',ProdController.addproducts)
-router.get('/',ProdController.index)
+router.get('/showproduct',ProdController.index)
 
 module.exports=router
